cb44e2b5e458f9d3400f129d6bed951bae43c3eb
AuroraN <contact@gregory-gaudin.com>
2017-04-28 22:33:31 +0200

    Fix bug («;» missing)

--- core/controllers/DefaultCore.php
+++ core/controllers/DefaultCore.php
@@ -29,7 +29,7 @@ class DefaultCoreController extends CoreController{
 				'icon'=>'',
 				'id'=>'updateAuwa',
 				'class'=>'fa fa-refresh'
-			))
+			));
 		$this->insertHeaderTab(array(
 			'icon'=>'',
 			'class'=>'fa fa-user UserAccount'
